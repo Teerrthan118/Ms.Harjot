80cbf28b85c74184a077a8ad60a8dd034302fe6f
Teerrthan118 <94755143+Teerrthan118@users.noreply.github.com>
2022-02-19 09:41:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+function changeT1() {
+    document.getElementById("T1").style.color = "red";
+}
+function changeH1() {
+    document.getElementById("H1").style.color = "orange";
+}
+function changeA1() {
+    document.getElementById("A1").style.color = "yellow";
+}
+function changeN1() {
+    document.getElementById("N1").style.color = "#00bf0c";
+}
+function changeK1() {
+    document.getElementById("K1").style.color = "blue";
+}
+function changeY1() {
+    document.getElementById("Y1").style.color = "purple";
+}
+function changeO1() {
+    document.getElementById("O1").style.color = "red";
+}
+function changeU1() {
+    document.getElementById("U1").style.color = "orange";
+}
+function changeM2() {
+    document.getElementById("M2").style.color = "yellow";
+}
+function changeS2() {
+    document.getElementById("S2").style.color = "#00bf0c";
+}
+function changePer() {
+    document.getElementById(".2").style.color = "blue";
+}
+function changeH2() {
+    document.getElementById("H2").style.color = "purple";
+}
+function changeA2() {
+    document.getElementById("A2").style.color = "red";
+}
+function changeR2() {
+    document.getElementById("R2").style.color = "orange";
+}
+function changeJ2() {
+    document.getElementById("J2").style.color = "yellow";
+}
+function changeO2() {
+    document.getElementById("O2").style.color = "#00bf0c";
+}
+function changeT2() {
+    document.getElementById("T2").style.color = "blue";
+}
+function change1() {
+    document.getElementById("!").style.color = "purple";
+}
+function reset() {
+    document.getElementById("T1").style.color = "black";
+    document.getElementById("H1").style.color = "black";
+    document.getElementById("A1").style.color = "black";
+    document.getElementById("N1").style.color = "black";
+    document.getElementById("K1").style.color = "black";
+    document.getElementById("Y1").style.color = "black";
+    document.getElementById("O1").style.color = "black";
+    document.getElementById("U1").style.color = "black";
+    document.getElementById("M2").style.color = "black";
+    document.getElementById("S2").style.color = "black";
+    document.getElementById(".2").style.color = "black";
+    document.getElementById("H2").style.color = "black";
+    document.getElementById("A2").style.color = "black";
+    document.getElementById("R2").style.color = "black";
+    document.getElementById("J2").style.color = "black";
+    document.getElementById("O2").style.color = "black";
+    document.getElementById("T2").style.color = "black";
+    document.getElementById("!").style.color = "black";
+}
+function open_image() {
+    document.getElementById("img1").style.display = "inline-block";
+    document.getElementById("p1").style.display = "inline-block";
+    document.getElementById("open_image").style.display = "none";
+    document.getElementById("close_image").style.display = "inline-block";
+}
+function close_image() {
+    document.getElementById("img1").style.display = "none";
+    document.getElementById("p1").style.display = "none";
+    document.getElementById("close_image").style.display = "none";
+    document.getElementById("open_image").style.display = "inline-block";
+}
